ba8ee87c277559d2de8c5ab3b3585d69e1cad282
ronnield <ronniel.appleid@gmail.com>
2024-11-11 07:55:54 +0800

    fix landing page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <h1>TestHive</h1>
+                    <h1>TaskHive</h1>
                 </a>
             </div>
 
