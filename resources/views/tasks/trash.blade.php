31cb43cfc6a027f8d6e72d6cb1d03f77f6a28bda
ronnield <ronniel.appleid@gmail.com>
2024-11-16 20:31:53 +0800

    test to deploy

--- resources/views/tasks/trash.blade.php
+++ resources/views/tasks/trash.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Trash') <!-- This should override the default title -->
 
 @section('content')
+
 <header class="container my-4 p-4 border rounded bg-light position-relative">
     <div class="row align-items-center">
         <div class="col-md-8">
